4a4e069f8b2dadd0c7a5549ace1d57869519c8da
Michal Domzalski <mimi0192@wp.pl>
2021-03-25 22:38:45 +0100

    fixed calculator not displaying properly

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -28,20 +28,20 @@ function Calculator() {
             <p className="has-text-white is-size-6 mb-2 has-text-centered">
                 {i18n.t('calculator')}
             </p>
-            <form>
+            <>
                 <input type="text" value={i18n.t('sum') + sum.toFixed(2) + ' zł'} className="input is-static is-disabled p-4 has-text-right has-background-light"/>
-                <div className="level is-mobile mt-4">
-                    <div className="level-item mr-5">
+                <div className="columns is-mobile mt-3">
+                    <div className="column is-half">
                         <input type="number" className="input" onClick={(e) => {e.target.value=''}} value={price} onChange={(e) => setPrice(e.target.value)} placeholder={i18n.t('item_price')}/>   
                     </div>
-                    <div className="level-item mr-5">
+                    <div className="column is-one-quarter">
                         <input type="button" value="-" className="button is-danger is-fullwidth" onClick={(e) => handleSubmit(e)}/>
                     </div>
-                    <div className="level-item">
+                    <div className="column is-one-quarter">
                         <input type="submit" value="+" className="button is-success is-fullwidth" onClick={(e) => handleSubmit(e)}/>
                     </div>
                 </div>
-            </form>
+            </>
         </div>
     )
 }
