289802889ae22e83bdb6ab3f4331fd8719c1d5f4
Michal Domzalski <mimi0192@wp.pl>
2021-03-25 22:25:34 +0100

    deleted the form

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -28,20 +28,18 @@ function Calculator() {
             <p className="has-text-white is-size-6 mb-2 has-text-centered">
                 {i18n.t('calculator')}
             </p>
-            <form>
-                <input type="text" value={i18n.t('sum') + sum.toFixed(2) + ' zł'} className="input is-static is-disabled p-4 has-text-right has-background-light"/>
-                <div className="level is-mobile mt-4">
-                    <div className="level-item mr-5">
-                        <input type="number" className="input" onClick={(e) => {e.target.value=''}} value={price} onChange={(e) => setPrice(e.target.value)} placeholder={i18n.t('item_price')}/>   
-                    </div>
-                    <div className="level-item mr-5">
-                        <input type="button" value="-" className="button is-danger is-fullwidth" onClick={(e) => handleSubmit(e)}/>
-                    </div>
-                    <div className="level-item">
-                        <input type="submit" value="+" className="button is-success is-fullwidth" onClick={(e) => handleSubmit(e)}/>
-                    </div>
+            <input type="text" value={i18n.t('sum') + sum.toFixed(2) + ' zł'} className="input is-static is-disabled p-4 has-text-right has-background-light"/>
+            <div className="level is-mobile mt-4">
+                <div className="level-item mr-5">
+                    <input type="number" className="input" onClick={(e) => {e.target.value=''}} value={price} onChange={(e) => setPrice(e.target.value)} placeholder={i18n.t('item_price')}/>   
                 </div>
-            </form>
+                <div className="level-item mr-5">
+                    <input type="button" value="-" className="button is-danger is-fullwidth" onClick={(e) => handleSubmit(e)}/>
+                </div>
+                <div className="level-item">
+                    <input type="submit" value="+" className="button is-success is-fullwidth" onClick={(e) => handleSubmit(e)}/>
+                </div>
+            </div>
         </div>
     )
 }
